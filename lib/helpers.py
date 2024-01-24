9d090828fe6d52dda739c603d464bd2893a6668e
elpatron <aidanseantucker@gmail.com>
2024-01-24 12:58:50 -0500

    added some mock data for users and tasks into company.db file

---
+++ lib/helpers.py
@@ -0,0 +1,43 @@
+from db.models import User
+from db.models import Task
+
+
+def exit_program():
+    print("Goodbye!")
+    exit()
+
+def create_user(name, department):
+    return User.create(name=name, department=department)
+
+def delete_user(user_id):
+    User.get(User.id == user_id).delete_instance()
+
+def get_all_users():
+    return User.select()
+
+def find_users_by_department(department):
+    return User.select().where(User.department == department)
+
+# ORM methods for Task
+def create_task(department, length_to_complete, description, user_id):
+    return Task.create(
+        department=department,
+        length_to_complete=length_to_complete,
+        description=description,
+        user=user_id
+    )
+
+def delete_task(task_id):
+    Task.get(Task.id == task_id).delete_instance()
+
+def get_all_tasks():
+    return Task.select()
+
+def find_tasks_by_department(department):
+    return Task.select().where(Task.department == department)
+
+def find_task_by_id(task_id):
+    return Task.get(Task.id == task_id)
+
+def find_user_by_id(user_id):
+    return User.get(User.id == user_id)
