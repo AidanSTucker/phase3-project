d36a3ee0a3fb10a72e5df1f9b95181b6de0069ac
elpatron <aidanseantucker@gmail.com>
2024-02-05 15:16:02 -0500

    modified user and task classes

--- lib/db/task.py
+++ lib/db/task.py
@@ -5,8 +5,10 @@ from db.__init__ import CURSOR, CONN
 class Task:
     all = {}
 
-    def __init__(self, name, department, id=None):
+    def __init__(self, id, department, length_to_complete, description, user_id=None):
         self.id = id
-        self.name = name
         self.department = department
+        self.length_to_complete = length_to_complete
+        self.description = description
+        self.user_id = user_id
         
--- lib/db/user.py
+++ lib/db/user.py
@@ -4,9 +4,8 @@ from db.__init__ import CURSOR, CONN
 class User:
     all = {}
 
-    def __init__(self, id, department, length_to_complete, description, user_id=None):
+    def __init__(self, id, department, length_to_complete, user_id=None):
         self.id = id
         self.department = department
         self.length_to_complete = length_to_complete
         self.user_id = user_id
-        
