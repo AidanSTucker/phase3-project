359e5c20ff414175a87fe8d00fa854926c5b75f2
elpatron <aidanseantucker@gmail.com>
2024-02-13 12:38:45 -0500

    removed input statements from cli.py file

--- lib/cli.py
+++ lib/cli.py
@@ -7,7 +7,6 @@ from helpers import (
     create_task,
     delete_task,
     get_all_tasks,
-    find_tasks_by_department,
     find_task_by_id,
     find_user_by_id,
     find_tasks_by_user
@@ -24,37 +23,22 @@ def main():
             get_all_tasks()
         elif choice == "2":
             get_all_users()
-        # elif choice == "3":
-        #     department = input("Enter department name: ")
-        #     find_tasks_by_department(department)
-        elif choice == "4":
-            department = input("Enter department name: ")
-            find_users_by_department(department)
-        elif choice == "5": ##department, length_to_complete, description, user_id
-            new_task_department = input("Enter in the department of the new task: ")
-            new_task_length_to_complete = input("How long will this task take to complete: ")
-            new_task_description = input("Enter in the description of this task: ")
-            new_task_user_id = input("Enter in the id of the user you'd like to assign this task to: ")
-            create_task(new_task_department, new_task_length_to_complete, new_task_description, new_task_user_id)
+        elif choice == "3":
+            find_users_by_department()
+        elif choice == "4": 
+            create_task()
+        elif choice == "5":
+            delete_task()
         elif choice == "6":
-            task_id = input("Enter the id of the task you would like to remove: ")
-            delete_task(task_id)
+            create_user()
         elif choice == "7":
-            new_user_name = input("Enter new users name: ")
-            new_user_department = input("Enter new users department: ")
-            create_user(new_user_name, new_user_department)
+            delete_user()
         elif choice == "8":
-            user = input("Enter the name of the user you would like to remove: ")
-            delete_user(user)
+            find_task_by_id()
         elif choice == "9":
-            id = input("Enter Tasks id: ")
-            find_task_by_id(id)
+            find_user_by_id()
         elif choice == "10":
-            id = input("Enter Users id: ")
-            find_user_by_id(id)
-        elif choice == "11":
-            user_id = input("Enter in the id of the user you're searching for: ")
-            find_tasks_by_user(user_id)
+            find_tasks_by_user()
         elif choice.isdigit():
             print("Invalid choice, please enter in a number shown on the menu: ")
         else:
@@ -66,15 +50,14 @@ def menu():
     print("0. Exit the program")
     print("1. View all tasks")
     print("2. View all users")
-    print("3. View tasks by department")
-    print("4. View users by department")
-    print("5. Add new task")
-    print("6. Delete task")
-    print("7. Add new user")
-    print("8. Delete user")
-    print("9. Find task by id")
-    print("10. Find user by id")
-    print("11. Find all tasks owned by a user")
+    print("3. View users by department")
+    print("4. Add new task")
+    print("5. Delete task")
+    print("6. Add new user")
+    print("7. Delete user")
+    print("8. Find task by id")
+    print("9. Find user by id")
+    print("10. Find your active tasks")
 
 
 if __name__ == "__main__":
--- lib/helpers.py
+++ lib/helpers.py
@@ -17,6 +17,8 @@ def exit_program():
             print("Error: Value entered is not a number, please try again.")
 
 def create_user(name, department):
+    name = input("Enter new users name: ")
+    department = input("Enter new users department: ")
     try:
         user = User.create(name, department)
         print(f"Success: {user}")
@@ -24,6 +26,7 @@ def create_user(name, department):
         print("Error creating user: ", exc)
 
 def delete_user(name):
+    name = input("Enter the name of the user you would like to remove: ")
     if user := User.find_by_name(name):
         user.delete()
         print(f"User {name} deleted")
@@ -41,7 +44,8 @@ def get_all_users():
         print("")
     print("--------------------------")
 
-def find_users_by_department(department):     ## --> Needs work
+def find_users_by_department(department): 
+    department = input("Enter department name: ")    ## --> Needs work
     users = User.find_by_department(department)
     if users:
         print(f"All Users in Department '{department}':\n")
@@ -54,14 +58,19 @@ def find_users_by_department(department):     ## --> Needs work
     else:
         print(f"No users found in department '{department}'.")
 
-def create_task(department, length_to_complete, description, user_id):
+def create_task():
+    description = input("Enter in the description of this task: ")
+    length_to_complete = input("How long will this task take to complete: ")
+    user_id = input("Enter in the id of the user you'd like to assign this task to: ")
     try:
-        task = Task.create(department, length_to_complete, description, user_id)
+        task = Task.create(length_to_complete, description, user_id)
         print(f"Task created: {task}")
     except Exception as exc:
         print("Error creating task: ", exc)
 
+
 def delete_task(task_id):
+    task_id = input("Enter the id of the task you would like to remove: ")
     if task := Task.find_by_id(task_id):
         task.delete()
         print(f"Task {task_id} deleted")
@@ -73,21 +82,22 @@ def get_all_tasks():
     for task in tasks:
         print(task)
 
-def find_tasks_by_department(department):
-    tasks = Task.find_by_department(department)
-    if tasks:
-        print(f"All Tasks in Department '{department}':\n")
-        print("--------------------------")
-        for task in tasks:
-            print("- Description:", task.description)
-            print("  - Length to Complete:", task.length_to_complete)
-            print("  - User ID Assigned:", task.user_id)
-            print("")
-        print("--------------------------")
-    else:
-        print(f"No tasks found in department '{department}'.")
+# def find_tasks_by_department(department):
+#     tasks = Task.find_by_department(department)
+#     if tasks:
+#         print(f"All Tasks in Department '{department}':\n")
+#         print("--------------------------")
+#         for task in tasks:
+#             print("- Description:", task.description)
+#             print("  - Length to Complete:", task.length_to_complete)
+#             print("  - User ID Assigned:", task.user_id)
+#             print("")
+#         print("--------------------------")
+#     else:
+#         print(f"No tasks found in department '{department}'.")
 
-def find_task_by_id(task_id):
+def find_task_by_id():
+    task_id = input("Enter Tasks id: ")
     if task := Task.find_by_id(task_id):
         print("--------------------------")
         print("- Description:", task.description)
@@ -99,7 +109,8 @@ def find_task_by_id(task_id):
     else:
         print("No task found with the specified ID.")
 
-def find_user_by_id(user_id):
+def find_user_by_id():
+    user_id = input("Enter Users id: ")
     if user := User.find_by_id(user_id):
         print("--------------------------")
         print("- Name: ", user.name)
@@ -109,14 +120,20 @@ def find_user_by_id(user_id):
     else:
         print("No user found with the specified ID.")
 
-def find_tasks_by_user(user_id):
-    tasks = Task.find_by_user_id(user_id)
-    if tasks:
-        print("--------------------------")
-        for task in tasks:
-            print("- Description:", task.description)
-            print("  - Length to Complete:", task.length_to_complete)
-            print("")
-        print("--------------------------")
+def find_tasks_by_user():
+    user_name = input("Enter in your name: ")
+    user = User.find_by_name(user_name)
+    if user:
+        user_id = user.id
+        tasks = Task.find_by_user_id(user_id)
+        if tasks:
+            print("--------------------------")
+            for task in tasks:
+                print("- Description:", task.description)
+                print("  - Length to Complete:", task.length_to_complete)
+                print("")
+            print("--------------------------")
+        else:
+            print("No tasks found for the specified user.")
     else:
-        print("No tasks found for the specified user ID.")
+        print("User not found.")
